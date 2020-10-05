109350c4e88befcc8748aedf5512b9aa01fdf717
Javachenxu <chenxu135790@outlook.com>
2020-10-05 10:57:59 +0800

    测试类

--- 05-队列/src/com/cx/Main.java
+++ 05-队列/src/com/cx/Main.java
@@ -1,7 +1,30 @@
 package com.cx;
 
+import com.cx.circle.CircleQueue;
+
 public class Main {
+	
+	static void test2() {
+		CircleQueue<Integer> queue = new CircleQueue<>();
+		
+		for (int i = 0; i < 10; i++) {
+			queue.enQueue(i);
+		}
+		for (int i = 0; i < 5; i++) {
+			queue.deQueue();
+		}
+		for (int i = 15; i < 23; i++) {
+			queue.enQueue(i);
+		}
+		System.out.println(queue);
+		while (!queue.isEmpty()) {
+			System.out.println(queue.deQueue());
+		}
+	}
+	
 	public static void main(String[] args) {
+		
+		test2();
 		
 	/*	Queue<Integer> queue = new Queue<>();
 		queue.enQueue(11);
@@ -12,7 +35,7 @@ public class Main {
 		
 		while(!queue.isEmpty()) {
 			System.out.println(queue.deQueue());
-		}*/
+		}
 		
 		Deque<Integer> queue = new Deque<>();
 		queue.enQuenefront(11);
@@ -23,6 +46,6 @@ public class Main {
 		
 		while(!queue.isEmpty()) {
 			System.out.println(queue.deQueueFront());
-		}
+		}*/
 	}
 }
