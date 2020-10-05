c5cd64f0ba91664842f1e17ad9707f078f784711
Javachenxu <chenxu135790@outlook.com>
2020-10-05 09:49:04 +0800

    双端队列

--- 05-队列/src/com/cx/Main.java
+++ 05-队列/src/com/cx/Main.java
@@ -3,7 +3,7 @@ package com.cx;
 public class Main {
 	public static void main(String[] args) {
 		
-		Queue<Integer> queue = new Queue<>();
+	/*	Queue<Integer> queue = new Queue<>();
 		queue.enQueue(11);
 		queue.enQueue(22);
 		queue.enQueue(33);
@@ -12,7 +12,17 @@ public class Main {
 		
 		while(!queue.isEmpty()) {
 			System.out.println(queue.deQueue());
-		}
+		}*/
 		
+		Deque<Integer> queue = new Deque<>();
+		queue.enQuenefront(11);
+		queue.enQuenefront(22);
+		queue.enQueueRear(33);
+		queue.enQueueRear(44);
+		//System.out.println(queue.deQueueFront());
+		
+		while(!queue.isEmpty()) {
+			System.out.println(queue.deQueueFront());
+		}
 	}
 }
