a16f1b86173e1a560916ba6b2a0a461c564f8879
Javachenxu <chenxu135790@outlook.com>
2020-10-11 16:56:49 +0800

    前序遍历非递归两种思路

--- 07-二叉搜索树/src/com/cx/BinarySearchtree.java
+++ 07-二叉搜索树/src/com/cx/BinarySearchtree.java
@@ -2,6 +2,7 @@ package com.cx;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 import com.cx.printer.BinaryTreeInfo;
 
@@ -50,6 +51,55 @@ public class BinarySearchtree<E> implements BinaryTreeInfo {//调用打印器
 		preorderTraversal(node.leaf);
 		preorderTraversal(node.right);
 		
+	}
+	
+	//前序遍历非递归实现(栈)
+	/*思路一：
+	 * 根节点存入栈中打印根节点，然后访问这个根节点的左子树，左子树也是将左子树的根存入栈中打印根节点，
+	 * 依次往下直到左子树为空，再取出栈顶元素，栈顶元素(访问完左子树的根节点)作为新的根节点去访问右子树。
+	 */
+	
+	public void preorderTraversal1() {
+		Node<E> node = root;//这样设置，而非将root入栈，可以代表每个完整树的开始
+		if (node == null) return;//边界判断
+		
+		Stack<E> stack = new Stack<>();
+		System.out.println("前序遍历为");
+		while (!stack.isEmpty() || node != null) {
+			 //沿着p的左指针非空进栈走到最左，最左边可能有两种情况，最左子树无左右子树和最左子树只有右子树
+			while (node != null) {
+				
+				stack.push((E) node);//前序遍历，先访问节点，
+				System.out.print(node.element+ "_");//打印根节点
+				node = node.leaf;//访问左子树
+			}
+				node = (Node<E>) stack.pop();//栈顶元素出栈
+				node = node.right;//栈顶元素作为新的根节点访问右子树
+		}
+	}
+	
+	/*
+	 * 思路二：我们知道前序遍历遵从根左右的顺序，所以我们打印根节点，并将它的右子树，左子树按照顺序压入栈中，
+	 * 每次只取出栈顶的一个节点，这样就可以保证所有的左子树都会在右子树之前取出并打印。
+	 */
+	public void preorderTraversal2() {
+		Stack<E> stack = new Stack<>();
+		
+		if (root == null) return;
+		
+		stack.push((E) root);
+		System.out.println("前序遍历为：");
+		while (!stack.isEmpty()) {
+			Node<E> node  = (Node<E>) stack.pop();
+			System.out.print(node.element +"_");
+			//栈先进后出，所以先让右子树入栈
+			if (node.right != null) {
+				stack.push((E) node.right);
+			}
+			if (node.leaf != null) {
+				stack.push((E) node.leaf);
+			}
+		}
 	}
 	
 	//二叉树中序遍历(递归实现)
