8f591219f3cde290254cd9a2938fe6472610aa58
Javachenxu <chenxu135790@outlook.com>
2020-10-14 00:10:37 +0800

    二叉树的前序遍历(迭代实现使用栈)

--- 00-LeetCode刷题训练/src/二叉树/_144_二叉树的前序遍历.java
+++ 00-LeetCode刷题训练/src/二叉树/_144_二叉树的前序遍历.java
@@ -1,5 +1,41 @@
 package 二叉树;
 
-public class _144_二叉树的前序遍历 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+public class _144_二叉树的前序遍历 {
+	
+    public List<Integer> preorderTraversal(TreeNode root) {
+    	/*
+       //递归法
+       preOrder(root);
+       return result;
+    }
+    private List<Integer> result = new ArrayList<>();
+    private void preOrder(TreeNode root) {
+        if (root == null) return;
+        
+        preorderTraversal(root.left);
+        result.add(root.val);
+        preorderTraversal(root.right);
+    }*/
+    	//迭代(栈实现)
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return list;
+        TreeNode node = root;
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+    			stack.push(node.right);
+    		}
+    		if (node.left != null) {
+    			stack.push(node.left);
+    		}
+        }
+        return list;
+        }
 }
