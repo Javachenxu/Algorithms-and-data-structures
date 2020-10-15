ebca528b9efc7916894a9b377eb7d76aa61e932a
Javachenxu <chenxu135790@outlook.com>
2020-10-15 14:23:15 +0800

    统一旋转操作

--- 08-AVL树/src/com/cx/tree/AVLTree.java
+++ 08-AVL树/src/com/cx/tree/AVLTree.java
@@ -1,8 +1,17 @@
 package com.cx.tree;
 
+import java.util.Comparator;
+
 import com.cx.tree.BinaryTree.Node;
 
 public class AVLTree<E> extends BST<E> {
+	
+	public AVLTree() {
+		this(null);
+	}
+	public AVLTree(Comparator<E> comparator) {
+		super(comparator);
+	}
 	
 	protected void afterAdd(Node<E> node) {
 		while ((node = node.parent) != null) {
@@ -25,7 +34,7 @@ public class AVLTree<E> extends BST<E> {
 	 * 恢复平衡
 	 * @param grand 高度最低的那个不平衡节点
 	 */
-	private void rebalance(Node<E> grand) {
+	private void rebalance2(Node<E> grand) {
 		Node<E> parent = ((AVLNode<E>) grand).tallerChild();
 		Node<E> node = ((AVLNode<E>) parent).tallerChild();
 		if (parent.isLeftChild()) {//L
@@ -43,6 +52,68 @@ public class AVLTree<E> extends BST<E> {
 				rotateLeft(grand);
 			}
 		}
+	}
+	//统一旋转操作
+	private void rebalance(Node<E> grand) {
+		Node<E> parent = ((AVLNode<E>) grand).tallerChild();
+		Node<E> node = ((AVLNode<E>) parent).tallerChild();
+		if (parent.isLeftChild()) {//L
+			if (node.isLeftChild()) {//LL
+				rotate(grand, node.left, node, node.right, parent, parent.right, grand, grand.right);
+			} else {//LR
+				rotate(grand, parent.left, parent, node.left, node, node.right, grand, grand.right);
+			}
+		} else {//R
+			if (node.isLeftChild()) {//RL
+				rotate(grand, grand.left, grand, node.left, node, node.right, parent, parent.right);
+			} else {//RR
+				rotate(grand, grand.left, grand, parent.left, parent, node.left, node, node.right);
+				
+			}
+		}
+	}
+	
+	private void rotate(
+			Node<E> r,  //子树的根节点
+			Node<E> a,Node<E> b,Node<E> c,
+			Node<E> d,
+			Node<E> e,Node<E> f,Node<E> g) {
+		d.parent = r.parent;
+		if (r.isLeftChild()) {
+			r.parent.left = d;
+		} else if (r.isRightChild()) {
+			r.parent.right = d;
+		} else {
+			root = d;
+		}
+		//a-b-c
+		b.left = a;
+		if (a != null) {
+			a.parent = b;
+		}
+		b.right = c;
+		if (c != null) {
+			c.parent = b;
+		}
+		updateHeight(b);
+		//e-f-g
+		f.left = e;
+		if (e != null) {
+			e.parent = f;
+		}
+		f.right = g;
+		if (g != null) {
+			g.parent = f;
+		}
+		updateHeight(f);
+		
+		//b-d-f
+		d.left = b;
+		d.right = f;
+		b.parent = d;
+		f.parent = d;
+		
+		updateHeight(d);
 	}
 	//左旋转
 	private void rotateLeft(Node<E> grand) {
@@ -123,7 +194,8 @@ public class AVLTree<E> extends BST<E> {
 							
 		}
 
-		@Override
+		
+		
 		public String toString() {
 		
 			String parentString = "null";
@@ -131,10 +203,6 @@ public class AVLTree<E> extends BST<E> {
 				parentString = parent.element.toString();
 			}
 			return element + "_p(" + parentString + ")_h(" + height + ")";
-			
 		}
-		
-		
-		
 	}
 }
