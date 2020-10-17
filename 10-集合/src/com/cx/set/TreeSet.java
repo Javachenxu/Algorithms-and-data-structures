f9469de68a340e0d775aaba3d48ff7265a42c67a
Javachenxu <chenxu135790@outlook.com>
2020-10-17 19:34:42 +0800

    用红黑树实现集合

---
+++ 10-集合/src/com/cx/set/TreeSet.java
@@ -0,0 +1,51 @@
+package com.cx.set;
+
+import com.cx.tree.BinaryTree;
+import com.cx.tree.RBTree;
+
+public class TreeSet<E> implements Set<E> {
+	private RBTree<E> tree = new RBTree<>();
+	
+	public int size() {
+		
+		return tree.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		
+		return tree.isEmpty();
+	}
+
+	@Override
+	public void clear() {
+		tree.clear();
+	}
+
+	@Override
+	public boolean contains(E element) {
+		
+		return tree.contains(element);
+	}
+
+	@Override
+	public void add(E element) {//红黑树默认去重
+		tree.add(element);
+	}
+
+	@Override
+	public void remove(E element) {
+		tree.remove(element);
+	}
+
+	@Override
+	public void traversal(Visitor<E> visitor) {
+		tree.inorder(new BinaryTree.Visitor<E>() {
+			public boolean visit(E element) {
+				return visitor.visit(element);
+			}
+		});//中序遍历
+
+	}
+
+}
