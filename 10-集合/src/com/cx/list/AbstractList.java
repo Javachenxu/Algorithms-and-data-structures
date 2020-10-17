6d9403d673c9defb6e6c050c34dbc404d3311d23
Javachenxu <chenxu135790@outlook.com>
2020-10-17 19:35:52 +0800

    链表设计

---
+++ 10-集合/src/com/cx/list/AbstractList.java
@@ -0,0 +1,42 @@
+package com.cx.list;
+
+
+
+public abstract class AbstractList<E> implements List<E> {
+	
+	protected int size;
+	
+	public int size() {
+		return size ;
+	}
+	@Override
+	public boolean isEmpty() {
+		
+		return size == 0;
+	}
+	@Override
+	public void add(E element) {
+		add(size, element);
+	}
+	@Override
+	public boolean contains(E element) {
+		return indexOf(element) != ELEMENT_NOT_FOUND;
+	}
+	
+	protected void outOfBounds(int index) {
+		throw new IndexOutOfBoundsException("Index:" + index + "Size" + size);
+	}
+	//检查范围
+	protected void rangeCheck(int index) {
+		if(index < 0 || index >= size) {
+			outOfBounds(index);
+		}
+	}
+	//添加操作检查范围
+	protected void rangeCheckForAdd(int index) {
+		if (index < 0 || index > size) {
+			outOfBounds(index);
+		}
+	}
+
+}
