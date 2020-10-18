d4befa8dcd9a66c64278750d93e5dfb49d56e569
Javachenxu <chenxu135790@outlook.com>
2020-10-18 15:22:45 +0800

    修改bug

--- 11-映射/src/com/cx/Main.java
+++ 11-映射/src/com/cx/Main.java
@@ -1,7 +1,26 @@
 package com.cx;
 
+import com.cx.map.Map;
+import com.cx.map.Map.Visitor;
+import com.cx.map.TreeMap;
+
 public class Main {
-	public static void main(String args[]) {
+	
+	static void test1() {
+		Map<String, Integer> map = new TreeMap<>();
+		map.put("class", 2);
+		map.put("public", 5);
+		map.put("text", 6);
+		map.put("public", 8);
 		
+		map.traversal(new Visitor<String,Integer>() {
+			public boolean visit(String key, Integer value) {
+				System.out.println(key + "_" + value);
+				return false;
+			}
+		});
+	}
+	public static void main(String args[]) {
+		test1();
 	}
 }
