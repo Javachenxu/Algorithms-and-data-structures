8088e87cadf01507d7ede06d31f203a201eb75bb
Javachenxu <chenxu135790@outlook.com>
2020-10-18 16:29:38 +0800

    用map实现set

---
+++ 11-映射/src/com/cx/set/Set.java
@@ -0,0 +1,20 @@
+package com.cx.set;
+
+
+
+public interface Set<E> {
+	int size();
+	boolean isEmpty();
+	void clear();
+	boolean contains(E element);
+	void add(E element);
+	void remove(E element);
+	//遍历接口
+	void traversal(Visitor<E> visitor);
+	
+	public static abstract class Visitor<E> {
+		boolean stop;
+		public abstract boolean visit(E element);
+	}
+}
+
---
+++ 11-映射/src/com/cx/set/TreeSet.java
@@ -0,0 +1,47 @@
+package com.cx.set;
+
+import com.cx.map.Map;
+import com.cx.map.TreeMap;
+
+public class TreeSet<E> implements Set<E> {
+	Map<E, Object> map = new TreeMap<>();
+	public int size() {
+		return map.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	@Override
+	public void clear() {
+		map.clear();
+	}
+
+	@Override
+	public boolean contains(E element) {
+		
+		return map.containsKey(element);
+	}
+
+	@Override
+	public void add(E element) {
+		map.put(element, null);
+	}
+
+	@Override
+	public void remove(E element) {
+		map.remove(element);
+	}
+
+	@Override
+	public void traversal(Visitor<E> visitor) {
+		map.traversal(new Map.Visitor<E, Object>() {
+			public boolean visit(E key, Object value) {
+				return visitor.visit(key);
+			}
+		});
+	}
+
+}
