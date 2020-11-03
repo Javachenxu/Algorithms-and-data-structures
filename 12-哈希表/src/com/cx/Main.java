1fb27d73cc5d10f040f5b7c8c3e3382389c603c9
Javachenxu <chenxu135790@outlook.com>
2020-11-03 23:15:04 +0800

    哈希表

--- 12-哈希表/src/com/cx/Main.java
+++ 12-哈希表/src/com/cx/Main.java
@@ -1,9 +1,15 @@
 package com.cx;
-
+/**
+ * 
+ * @author 24750
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		
+		int code = Float.floatToIntBits(10.8f);
+		System.out.println(code);
+		System.out.println(Integer.toBinaryString(code));
 
 	}
 
